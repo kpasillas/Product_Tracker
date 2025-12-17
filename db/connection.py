db6f2fbe7e7de5c1400bb3db650579633f2ba9ab
Kristopher Pasillas <kris@kris-p.net>
2025-12-16 16:23:19 -0800

    Added environment variables

--- db/connection.py
+++ db/connection.py
@@ -1,25 +1,24 @@
-#!/usr/bin/env python3
+import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
+
 
 def get_mysql_engine():
 
     connection_url = URL.create(
         "mysql+pymysql",
-        username="kpasillas",
-        password="jeri$cho-CREATE5-noose",
-        host="172.233.150.156",
+        username=os.getenv("MYSQL_USERNAME"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
         database="product_tracker",
-        # query={'charset': 'utf8'},
     )
     engine = create_engine(
         connection_url,
         pool_size=5,
         max_overflow=10,
         pool_pre_ping=True,
-        pool_recycle=3600
-        # isolation_level='AUTOCOMMIT'
+        pool_recycle=3600,
     )
 
     return engine
